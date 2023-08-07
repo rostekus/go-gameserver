89c9723eeb4d6ff8ad24f8b81acd3dcb8281554c
rostekus <rostyslavmosorov@gmail.com>
2023-08-07 19:10:10 +0200

    feat: move game funcs to snake and fruit

--- pkg/game/game.go
+++ pkg/game/game.go
@@ -32,55 +32,41 @@ func NewGame(w, h int) *Game {
 		fieldWidth:    w,
 		fieldHeight:   h,
 		sn:            NewRandomSnake(w, h),
-		frt:           NewRandomFruit(w, h),
+		frt:           NewRandomFruit(),
 		v:             velocity{1, 0},
 		score:         0,
 		onlinePlayers: make(map[string]Drawable),
 	}
-	game.onlinePlayers["player"] = NewRandomFruit(w, h)
+	game.frt.AddFruit(w, h)
 	return &game
 }
 
-func NewRandomSnake(maxX, maxY int) *Snake {
-	return &Snake{rand.Intn(maxX), rand.Intn(maxY)}
-}
-
-func NewRandomFruit(maxX, maxY int) *Fruit {
-	return &Fruit{rand.Intn(maxX), rand.Intn(maxY)}
-}
-func (g *Game) isFruitEaten() bool {
-	if g.sn.x == g.frt.x && g.sn.y == g.frt.y {
-		return true
-	}
-	return false
-}
 func (g *Game) incScore() {
 	g.score++
 }
-func (g *Game) newFruit() {
-	g.frt.y = rand.Intn(g.fieldHeight)
-	g.frt.x = rand.Intn(g.fieldWidth)
-}
 func (g *Game) draw() {
 	termbox.Clear(snakeFgColor, snakeBgColor)
-	if g.isFruitEaten() {
+	headx, heady := g.sn.pos[0].x, g.sn.pos[0].y
+	if g.frt.EatFruit(headx, heady) {
 		g.incScore()
-		g.newFruit()
+		g.sn.Eat()
+		g.frt.AddFruit(g.fieldWidth, g.fieldHeight)
 	}
 
-	g.sn.Draw()
 	g.drawObject(g.sn)
 	g.drawObject(g.frt)
 	g.drawPlayers()
 	termbox.Flush()
 }
 func (g *Game) drawObject(object Drawable) {
-	termbox.SetCell(object.Draw())
+	for _, obj := range object.Draw() {
+		termbox.SetCell(obj.x, obj.y, obj.body, obj.fg, obj.bg)
+	}
 
 }
 
 func (g *Game) AddOnlinePlayer(pos []int32, username string) {
-	g.onlinePlayers[username] = &Fruit{int(pos[0]), int(pos[1])}
+	g.onlinePlayers[username] = CreateOtherPlayerFromSlice(pos)
 }
 
 func (g *Game) drawPlayers() {
@@ -90,11 +76,11 @@ func (g *Game) drawPlayers() {
 }
 
 func (g *Game) moveSnake() {
-	if g.sn.x+g.v.x < g.fieldWidth && g.sn.x+g.v.x > 0 {
-		if g.sn.y+g.v.y < g.fieldHeight && g.sn.y+g.v.y > 0 {
-			x := g.sn.x + g.v.x
-			y := g.sn.y + g.v.y
-			g.sn.Move(x, y)
+	g.sn.v = g.v
+	headx, heady := g.sn.pos[0].x, g.sn.pos[0].y
+	if headx+g.v.x < g.fieldWidth && headx+g.v.x > 0 {
+		if heady+g.v.y < g.fieldHeight && heady+g.v.y > 0 {
+			g.sn.Move()
 		}
 	}
 }
@@ -104,8 +90,13 @@ func (g *Game) step() {
 	g.draw()
 }
 
-func (g *Game) GetPosSnake() (int, int) {
-	return g.sn.x, g.sn.y
+func (g *Game) GetPosSnake() []int32 {
+	snakePos := make([]int32, 2*len(g.sn.pos))
+	for i := 0; i < len(g.sn.pos); i += 2 {
+		snakePos[i] = int32(g.sn.pos[i].x)
+		snakePos[i+1] = int32(g.sn.pos[i].y)
+	}
+	return snakePos
 }
 
 func (g *Game) moveLeft()  { g.v = velocity{-1, 0} }
